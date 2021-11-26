bcf8b345150fea00ac40156d61b7448968d20d35
정한길 <hg9587@naver.com>
2021-11-26 07:07:34 +0000

    nested structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # codedit-ui-js
+
+## References
+### CSS & HTML Layout related
+- https://stackoverflow.com/questions/29517722/html-align-div-children-horizontally
--- js/windowFrame.js
+++ js/windowFrame.js
@@ -1,5 +1,31 @@
-function WindowFrame() {
+function Item(content) {
+	this.content = content || "Hello javascript";
 	this.render = function() {
-		return "Hello world";
+		return `<div class="item border">${this.content}</div>`;
 	}
+	return this;
+}
+
+function Frame() {
+	this.children = [];
+	this.vertical = false;
+	this.add = function(child) {
+		this.children.push(child);
+	}
+	this.get = function(index) {
+		return this.children[index];
+	}
+	this.render = function() {
+		if (this.children.length == 0) {
+			return `<div class="frame">Empty Container</div>`;
+		} else {
+			var html = "";
+			for (var i=0; i < this.children.length; i++) {
+				html += this.children[i].render();
+			}
+			return `<div class="${this.vertical ? "vertical" : "horizontal"}-container frame">${html}</div>`;
+		}
+	}
+	
+	return this;
 }
