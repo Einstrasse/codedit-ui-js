3a411b29b65fbe2c731d8e66295763ccde894283
정한길 <hg9587@naver.com>
2021-12-03 15:59:55 +0000

    strong binding between js object and html element, uniquely selectable item

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 vscode나 visual studio 같은 유용한 코드 에디터에서 제공하는 코드 윈도우 분할 기능, 크기조절기능, 위치 변경 기능 등의 유용한 기능을 웹에서 사용할 수 있도록 프레임워크 없이 순수 자바스크립트로 구현해보는 토이 프로젝트입니다.
 
+## Structure
+Frame과 Item이 있다. Frame은 안에 Item이나 다른 Frame을 포함할 수 있고, Item은 컨텐츠 그 자체이다.
+Frame은 아이템을 가로 또는 세로로만 배열할 수 있다.
+
 ## Features todo
 - [x] Represent hierarchical structure
 - [x] Selectable Component
-- [ ] Strong binding between javascript object and HTML element
+- [x] Uniquely selectable item in same Frame
+- [x] Strong binding between javascript object and HTML element
 - [ ] Relative Size representation
 - [ ] Component resize support
 - [ ] Component drag and relocation support
--- js/windowFrame.js
+++ js/windowFrame.js
@@ -1,20 +1,18 @@
-function clickEvtListener(e) {
-	var target = e.target;
-	if (target && target.classList && target.classList.contains('selectable')) {
-		if (target.classList.contains("selected")) {
-			target.classList.remove("selected");
-		} else {
-			target.classList.add("selected");
-		}
-			
-	}
-}
-
 function Item(content) {
 	this.parent = false; //detach시 parent 값 확인 필요
 	this.content = content || "Hello javascript";
 	this.render = function() {
-		return `<div class="item border selectable">${this.content}</div>`;
+		var dom = document.createElement('div');
+		dom.classList.add("item");
+		dom.classList.add("border");
+		dom.classList.add("selectable");
+		dom.innerText = this.content;
+		dom.addEventListener("click", (e) => {
+			if (this.parent && this.parent.select && typeof this.parent.select === 'function') {
+				this.parent.select(dom);
+			}
+		});
+		return dom;
 	}
 	return this;
 }
@@ -23,6 +21,19 @@ function Frame() {
 	this.parent = false; //detach시 parent 값 확인 필요
 	this.children = [];
 	this.vertical = false; // false 시 horizontal
+	this.selectedItem = false;
+	this.select = function(selectedItem) {
+		if (this.parent === false) { //Root Item에서만 수행함
+			var prev = this.selectedItem;
+			if (prev && prev.classList && prev.classList.contains("selected")) {
+				prev.classList.remove("selected");
+			}
+			this.selectedItem = selectedItem;
+			this.selectedItem.classList.add("selected");
+		} else if (typeof this.parent.select === 'function') {
+			this.parent.select(selectedItem);
+		}
+	}
 	this.add = function(child) {
 		child.parent = this;
 		this.children.push(child);
@@ -32,13 +43,18 @@ function Frame() {
 	}
 	this.render = function() {
 		if (this.children.length == 0) {
-			return `<div class="frame">Empty Container</div>`;
+			var dom = document.createElement('div');
+			dom.classList.add('frame');
+			dom.innerText = "Empty Container";
+			return dom;
 		} else {
-			var html = "";
-			for (var i=0; i < this.children.length; i++) {
-				html += this.children[i].render();
+			var dom = document.createElement('div');
+			dom.classList.add(`${this.vertical ? "vertical" : "horizontal"}-container`);
+			dom.classList.add('frame');
+			for (var i=0; i< this.children.length; i++) {
+				dom.appendChild(this.children[i].render());
 			}
-			return `<div class="${this.vertical ? "vertical" : "horizontal"}-container frame">${html}</div>`;
+			return dom;
 		}
 	}
 	
